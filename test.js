8ef3f4108cbb708775c289e77b8bce4a33b5fd1e
alvaropinot <alvaro@neatnait.com>
2016-03-20 02:05:53 +0100

    style(tests): fix linting alerts

--- test.js
+++ test.js
@@ -1,75 +1,85 @@
-var chai = require('chai'),
-  mocha = require('mocha');
+'use strict';
+let chai = require('chai');
+let mocha = require('mocha');
 
-var describe = mocha.describe,
-  beforeEach = mocha.beforeEach,
-  it = mocha.it,
-  expect = chai.expect;
+let describe = mocha.describe;
+let beforeEach = mocha.beforeEach;
+let it = mocha.it;
+let expect = chai.expect;
 
-var circularIterator = require('./');
+let circularIterator = require('./');
 
-describe('circular iterator', function () {
-  var arr, iterator;
+describe('circular iterator', function() {
+  let arr;
+  let iterator;
 
   beforeEach(function() {
-    arr = [1, 2, 3];
+    arr = ['foo', 'bar', 'buz'];
     iterator = circularIterator(arr);
   });
 
-  it('should be defined and a function', function () {
-    expect(circularIterator).to.be.a('function');
-  });
+  describe('public API', function() {
 
-  it('should return the following properties', function () {
-    var status = circularIterator(arr).next();
-    expect(status).to.have.property('value');
-    expect(status).to.have.property('done').that.is.a('boolean');
-  });
-
-  it('should return first item', function () {
-    expect(circularIterator(arr).next().value).to.equal(1);
-  });
-
-  it('should return all items in order', function () {
-    arr.map(function(number){
-      expect(iterator.next().value).to.equal(number);
-    });
-  });
-
-  it('should return first number when the end is reached', function () {
-    // first call for each number
-    arr.map(function(number){
-      expect(iterator.next().value).to.equal(number);
+    it('should be defined and a function', function() {
+      expect(circularIterator).to.be.a('function');
     });
 
-    // called again on the same iterator should return the same array elements.
-    arr.map(function(number){
-      expect(iterator.next().value).to.equal(number);
+    it('should return the following properties', function() {
+      let status = circularIterator(arr).next();
+
+      expect(status).to.have.property('value');
+      expect(status).to.have.property('done').that.is.a('boolean');
     });
+
   });
 
-  it('should not return `done` when the end is reached', function () {
-    arr.map(function(){
-      iterator.next();
+  describe('usage', function() {
+    it('should return first item', function() {
+      expect(circularIterator(arr).next().value).to.equal('foo');
     });
-    expect(iterator.next().done).to.be.false;
-  });
 
-  it('should work with no params', function () {
-    var iterator = circularIterator();
-    expect(iterator.next().value).to.be.undefined;
-  });
+    it('should return all items in order', function() {
+      arr.map(function(element) {
+        expect(iterator.next().value).to.equal(element);
+      });
+    });
 
-  it('should work with non array params', function () {
-    expect(circularIterator(2).next().value).to.be.undefined;
-    expect(circularIterator(true).next().value).to.be.undefined;
-    expect(circularIterator(false).next().value).to.be.undefined;
-    expect(circularIterator({a:2}).next().value).to.be.undefined;
-  });
+    it('should return first element when the end is reached', function() {
+      // first call for each element
+      arr.map(function(element) {
+        expect(iterator.next().value).to.equal(element);
+      });
 
-  it('should not allow external modifications', function () {
-    expect(iterator.next().value).to.be.equal(1);
-    arr[1] = 'foo';
-    expect(iterator.next().value).to.be.equal(2);
+      // called again on the same iterator should return the same array elements.
+      arr.map(function(element) {
+        expect(iterator.next().value).to.equal(element);
+      });
+    });
+
+    it('should not return `done` when the end is reached', function() {
+      arr.map(function() {
+        iterator.next();
+      });
+      expect(iterator.next().done).to.be.false;
+    });
+
+    it('should work with no params', function() {
+      let voidIterator = circularIterator();
+
+      expect(voidIterator.next().value).to.be.undefined;
+    });
+
+    it('should work with non array params', function() {
+      expect(circularIterator('foo').next().value).to.be.undefined;
+      expect(circularIterator(true).next().value).to.be.undefined;
+      expect(circularIterator(false).next().value).to.be.undefined;
+      expect(circularIterator({foo: 'foo'}).next().value).to.be.undefined;
+    });
+
+    it('should not allow external modifications', function() {
+      expect(iterator.next().value).to.be.equal('foo');
+      arr[1] = 'biz';
+      expect(iterator.next().value).to.be.equal('bar');
+    });
   });
 });
